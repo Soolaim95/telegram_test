75054b7c3f6e23c9cf9d72a40afcd7a3d5472e10
Elizaveta.Polyakova <polyakova.elizaveta95@mail.ru>
2022-05-06 06:23:33 +0300

    telegram test

--- src/test/java/tests/simple/NegativeTests.java
+++ src/test/java/tests/simple/NegativeTests.java
@@ -1,10 +1,19 @@
 package tests.simple;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NegativeTests {
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @Test
     void test00() {
         assertTrue(false);
--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -1,11 +1,20 @@
 package tests.simple;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SkippedTests {
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @Test
     @Disabled
     void test00() {
